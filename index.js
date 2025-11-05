b6dc93f13d04cc3353b1f16ddbd186041a4f4145
Waqas Awan <mindworks.agents@gmail.com>
2025-11-05 17:40:10 +0500

    firstproject

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World, here is myh first project.");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log("Hello World, here is myh first project.");
