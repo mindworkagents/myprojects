1821bd2d6e947f8a0dd913339bc73e2d9bfa9d41
Waqas Awan <mindworks.agents@gmail.com>
2025-11-09 14:25:25 +0500

    simple calculator with predefined values.

---
+++ calculator/calculator.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num1 = 18;
+let num2 = 6;
+console.log("Sum = ", num1 + num2);
+console.log("Difference = ", num1 - num2);
+console.log("Product = ", num1 * num2);
+console.log("Quotient = ", num1 / num2);
+console.log("Percentage = ", num2 / num1 * 100, "%");
+console.log("Code Ended");
+//# sourceMappingURL=calculator.js.map
---
+++ calculator/calculator.ts
@@ -0,0 +1,8 @@
+let num1= 18;
+let num2 = 6;
+console.log("Sum = ", num1 + num2);
+console.log("Difference = ", num1 - num2);
+console.log("Product = ", num1*num2);
+console.log("Quotient = ", num1/num2);
+console.log("Percentage = ", num2/num1*100,"%");
+console.log("Code Ended");
