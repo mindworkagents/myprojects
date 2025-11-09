2b5f366fdb4c685f60ed0c097c4ed08cbdafddf8
Waqas Awan <mindworks.agents@gmail.com>
2025-11-09 14:00:33 +0500

    biodata-form

---
+++ biodataform/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("      My Biodata       ");
+console.log("Name: Muhammad Waqas Ahmed");
+console.log("Age: 34");
+console.log("Gender: Male");
+console.log("Qualification: MSc Physics");
+console.log("Address: Bhawana, Chiniot Punjab Pakistan");
+console.log("Skills: Typescript, JavaScript, HTML, CSS");
+console.log("Hobbies: Reading, COding, Travelling.");
+console.log("Contact: +923324570707");
+console.log("Email: mindworks.agents@gmail.com");
+console.log("Thanks your information saved successfully.");
+//# sourceMappingURL=index.js.map
---
+++ biodataform/index.ts
@@ -0,0 +1,11 @@
+console.log("      My Biodata       ");
+console.log("Name: Muhammad Waqas Ahmed");
+console.log("Age: 34");
+console.log("Gender: Male");
+console.log("Qualification: MSc Physics");
+console.log("Address: Bhawana, Chiniot Punjab Pakistan");
+console.log("Skills: Typescript, JavaScript, HTML, CSS");
+console.log("Hobbies: Reading, COding, Travelling.");
+console.log("Contact: +923324570707");
+console.log("Email: mindworks.agents@gmail.com");
+console.log("Thanks your information saved successfully.");
